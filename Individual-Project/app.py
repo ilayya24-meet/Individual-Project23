7022d77e2b48a8c04698ed6340ab249cdf2ebb3a
ilayya24-meet <139681132+ilayya24-meet@users.noreply.github.com>
2023-07-26 17:26:58 +0300

    LeBron James

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -2,15 +2,92 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+firebaseConfig = {
+
+  "apiKey": "AIzaSyCDa9yRefjrbE_yA5YM5xWn5Rl4WZiCXG8",
+
+  "authDomain": "individual-cs-project-60727.firebaseapp.com",
+
+  "databaseURL": "https://individual-cs-project-60727-default-rtdb.europe-west1.firebasedatabase.app",
+
+  "projectId": "individual-cs-project-60727",
+
+  "storageBucket": "individual-cs-project-60727.appspot.com",
+
+  "messagingSenderId": "156668857907",
+
+  "appId": "1:156668857907:web:7c841de09a8a0cf3671d46",
+
+  "measurementId": "G-Y8SX4GKMWN"
+
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+db = firebase.database()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        username = request.form['username']
+        
+
+
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID = login_session['user']['localId']
+            user = {'email': email, 'password': password, 'firstname': firstname, 'lastname': lastname, 'username': username}
+            db.child("Users").child(UID).set(user)
+
+            return redirect(url_for('signin'))
+
+        except:
+           error = "Authentication failed"
+    return render_template("signup.html")
 
 
 
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+   error = ""
+   if request.method == 'POST':
+       print('posted')
+       email = request.form['email']
+       password = request.form['password']
+       try:
+           login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+           print('signed in')
+           return redirect(url_for('home'))
+       except Exception as e:
+            print(e)
+            error = "Authentication failed"
+   return render_template("signin.html")
 
-#Code goes above here
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    return render_template("home.html")
+
+@app.route('/mainpage', methods=['GET', 'POST'])
+def lepage():
+     if 'user' in login_session and login_session['user'] is not None:
+        return render_template("lepage.html")
+     else:
+        return redirect(url_for('home'))
+    
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
